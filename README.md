967a87b657bf5fd9244788a9eb99648de0a7eb9f
Juan Cruz Borssotto <jborssotto@gmail.com>
2020-12-14 22:34:51 -0300

    Add parallelization to multi price searching in GetPricesFor
    Add mutex to transparent cache to lock prices map reads and writes
    
    Add one channel for results and one for errors. Make calls to get each individual price in
    goroutines and insert in channels
    
    Select from channels and save results or return error if exists

--- README.md
+++ README.md
@@ -5,3 +5,6 @@
 * First TODO: noticed that a way to know when a price was cached was necessary so proceed
   to create a custom price struct. Updated function to save that time along the price itself and then
   implemented the if statement. Did a manual test in the main function to check everything was fine.
+* Second TODO: noticed that GetPriceFor was reading and writing a shared resource, so proceed to add
+  a mutex to lock before those operations. Then added the parallelization logic to GetPricesFor and same 
+  as before, tested it in the main function.
--- cache.go
+++ cache.go
@@ -1,8 +1,9 @@
 package sample1
 
 import (
-	"time"
 	"fmt"
+	"sync"
+	"time"
 )
 
 // PriceService is a service that we can use to get prices for the items
@@ -24,6 +25,7 @@ type TransparentCache struct {
 	actualPriceService PriceService
 	maxAge             time.Duration
 	prices             map[string]TransparentCachePrice
+	pricesMutex        sync.Mutex
 }
 
 func NewTransparentCache(actualPriceService PriceService, maxAge time.Duration) *TransparentCache {
@@ -31,12 +33,16 @@ func NewTransparentCache(actualPriceService PriceService, maxAge time.Duration) 
 		actualPriceService: actualPriceService,
 		maxAge:             maxAge,
 		prices:             map[string]TransparentCachePrice{},
+		pricesMutex:        sync.Mutex{},
 	}
 }
 
 // GetPriceFor gets the price for the item, either from the cache or the actual service if it was not cached or too old
+// Is safe for concurrent calls.
 func (c *TransparentCache) GetPriceFor(itemCode string) (float64, error) {
+	c.pricesMutex.Lock()
 	cachePrice, ok := c.prices[itemCode]
+	c.pricesMutex.Unlock()
 	if ok {
 		if time.Since(cachePrice.lastAssigment) < c.maxAge {
 			return cachePrice.value, nil
@@ -47,10 +53,12 @@ func (c *TransparentCache) GetPriceFor(itemCode string) (float64, error) {
 	if err != nil {
 		return 0, fmt.Errorf("getting price from service : %v", err.Error())
 	}
+	c.pricesMutex.Lock()
 	c.prices[itemCode] = TransparentCachePrice{
 		value:         price,
 		lastAssigment: time.Now(),
 	}
+	c.pricesMutex.Unlock()
 	return price, nil
 }
 
@@ -58,13 +66,25 @@ func (c *TransparentCache) GetPriceFor(itemCode string) (float64, error) {
 // If any of the operations returns an error, it should return an error as well
 func (c *TransparentCache) GetPricesFor(itemCodes ...string) ([]float64, error) {
 	results := []float64{}
+	resultsStream, errStream := make(chan float64), make(chan error)
 	for _, itemCode := range itemCodes {
-		// TODO: parallelize this, it can be optimized to not make the calls to the external service sequentially
-		price, err := c.GetPriceFor(itemCode)
-		if err != nil {
+		go func(internalItemCode string) {
+			price, err := c.GetPriceFor(internalItemCode)
+			if err != nil {
+				errStream <- err
+			} else {
+				resultsStream <- price
+			}
+		}(itemCode)
+	}
+
+	for range itemCodes {
+		select {
+		case result := <- resultsStream:
+			results = append(results, result)
+		case err := <- errStream:
 			return []float64{}, err
 		}
-		results = append(results, price)
 	}
 	return results, nil
 }
